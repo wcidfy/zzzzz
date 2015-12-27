da94fa526a134882bd0117c6210db964afb4ea08
lxx <lxx@lixiangxiangdemacbook-pro.local>
2015-12-27 15:42:18 +0800

    Initial Commit

---
+++ zzzzz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zzzzz
+//
+//  Created by lxx on 15/12/27.
+//  Copyright © 2015年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zzzzz/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  zzzzz
+//
+//  Created by lxx on 15/12/27.
+//  Copyright © 2015年 lxx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
